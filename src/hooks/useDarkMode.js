a54b0c8ea9afa63b1ff83c4e68ed6c334e6dd795
devin mitchell <devin.a.mitchell@gmail.com>
2021-05-05 14:57:30 -0600

    finished MVP

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = (key) => {
+    const [value, setValue] = useLocalStorage(key)
+
+    return([value, setValue])
+}
